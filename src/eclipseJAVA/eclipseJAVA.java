3b3e25c64df64e0816c16e6bd29dffea8830c20b
uoehoswi3 <thdus1821@naver.com>
2022-01-24 14:46:50 +0900

    third commit

--- src/eclipseJAVA/eclipseJAVA.java
+++ src/eclipseJAVA/eclipseJAVA.java
@@ -3,6 +3,6 @@ package eclipseJAVA;
 public class eclipseJAVA {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
-		System.out.println("Hi, Hello");
+		System.out.println("Hi, Hello!");
 	}
 }
